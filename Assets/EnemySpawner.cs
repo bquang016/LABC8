cc68844153fbf8774f2e49bdc18507b9b7f4afd8
bquang016 <buidangquangmtp@gmail.com>
2026-03-18 11:03:38 +0700

    labc8

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Biến này dùng để chứa cái khuôn (Prefab) quái vật mà ta muốn đúc ra
+    public GameObject enemyPrefab;
+
+    void Update()
+    {
+        // Kiểm tra xem người chơi có bấm phím Space không
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Triệu hồi (Instantiate) quái vật tại vị trí của cục Spawner
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
